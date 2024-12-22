c610c6ae0d89b03783611021dd146ab84903327e
Yhutee Elijah <Utibeelijah11@gmail.com>
2024-12-22 21:42:48 +0100

    added daily_reminder.py

--- control-flow.py/daily_reminder.py
+++ control-flow.py/daily_reminder.py
@@ -4,16 +4,16 @@ time_bound = input("Is it time-bound? (yes/no):")
 
 match priority:
    case "high" if time_bound == "yes":
-      print(f"{task} is a high priority task that requires immediate attention!")
+      print(f"Reminder: {task} is a high priority task that requires immediate attention!")
    case "high" if time_bound == "no":
-      print(f"{task} is a high priority task that does not require timely attention")
+      print(f"Reminder: {task} is a high priority task that does not require timely attention")
    case "medium" if time_bound == "yes":
-      print(f"{task} is a medium priority task that requires timely attention")
+      print(f"Reminder: {task} is a medium priority task that requires timely attention")
    case "medium" if time_bound == "no":
-      print(f"{task} is a medium priority task that does not require timely attention")
+      print(f"Reminder: {task} is a medium priority task that does not require timely attention")
    case "low" if time_bound == "high":
-      print(f"{task} is a low priority task that requires timely attention")
+      print(f"Reminder: {task} is a low priority task that requires timely attention")
    case "low" if time_bound == "no":
-      print(f"{task} is a low priority task that does not require timely attention")
+      print(f"Reminder: {task} is a low priority task that does not require timely attention")
    case _:
-      print("Invalid priority")
+      print("Reminder: Invalid priority")
