3dc43cf3c6ca414db8d2bc6a631a586e13a2096f
Yhutee Elijah <Utibeelijah11@gmail.com>
2025-01-13 18:51:03 +0100

    Update arithmetic_operations.py
    updated arithmetic_operations.py

--- fns_and_dns/arithmetic_operations.py
+++ fns_and_dns/arithmetic_operations.py
@@ -1,25 +1,13 @@
 def perform_operation(num1, num2, operation):
-    if operation == 'add':
-        return num1 + num2
-    elif operation == 'subtract':
-        return num1 - num2
-    elif operation == 'multiply':
-        return num1 * num2
-    elif operation == 'divide':
-        if num2 == 0:
-            return "Cannot divide by zero"
-        else:
-            return num1 / num2
-    else:
-        return "Invalid operation"
-    
-    def main():
-        print("Arithmetic Operations")
-        num1 = float(input("Enter the first number: "))
-        num2 = float(input("Enter the second number: "))
-        operation = input("Enter the operation to (add, subtract, multiply, divide): ").strip().lower()
-        result = perform_operation(num1, num2, operation)
-        print(f"Result is: {result}")   
-
-        if __name__ == "__main__":
-            main()
+    match operation:
+       case "add":
+           return num1 + num2
+        case "subtract":
+            return num1 - num2
+        case "multiply":
+            return num1 * num2
+        case "divide":
+            if num2 == 0:
+               print("Zero division not allowed")
+            elif:
+                return num1 / num2
