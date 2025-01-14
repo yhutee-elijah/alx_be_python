b8fbc1310113ea1af6a168c02da3eb9c3716976e
Yhutee Elijah <Utibeelijah11@gmail.com>
2025-01-14 16:56:16 +0100

    Update shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,6 +1,6 @@
 def display_menu():
     print("\nShopping List Manager")
-    print("1. Add items")
+    print("1. Add Items")
     print("2. Remove Item")
     print("3. View List")
     print("4. Exit")
