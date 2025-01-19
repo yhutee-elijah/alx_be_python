611fae79aca36ce11105ad82157ba7979fb5899a
Yhutee Elijah <Utibeelijah11@gmail.com>
2025-01-19 19:58:14 +0100

    Update test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,23 +1,25 @@
 import unittest
 from programming_paradigm.simple_calculator import SimpleCalculator
 
-
 class TestSimpleCalculator(unittest.TestCase):
-    def test_add(self):
-        calc = SimpleCalculator()
-        self.assertEqual(calc.add(1, 2), 3)
+    def setUp(self):
+      """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
 
-    def test_subtract(self):
-        calc = SimpleCalculator()
-        self.assertEqual(calc.subtract(2, 1), 1)
+        def test_addition(self):
+            """Test the addition method."""
+            self.assertEqual(self.calc.add(2, 3), 5)
+            self.assertEqual(self.calc.add(-1, 1), 0)
+            self.assertEqual(self.calc.add(0, 0), 0)
+            self.assertEqual(self.calc.add(-5, -3), -8)
+            self.assertEqual(self.calc.add(1.5, 2.5), 4.0)
+            self.assertEqual(self.calc.add(-1.5,-2.5), 4.0)
+            self.assertEqual(self.calc.add(1e10, 1e10), 2e10)
+            self.assertEqual(self.calc.add(-1e10, -1e10), -2e10)
+            self.assertEqual(self.calc.add(1e-10, 1e-10), 2e-10)
+            self.assertEqual(self.calc.add(-1e-10, -1e-10), -2e-10)
 
-    def test_multiply(self):
-        calc = SimpleCalculator()
-        self.assertEqual(calc.multiply(2, 3), 6)
-
-    def test_divide(self):
-        calc = SimpleCalculator()
-        self.assertEqual(calc.divide(6, 3), 2)
+        def test_subtraction(self):
 
         def test_edge_case(self):
                 calc = SimpleCalculator()
